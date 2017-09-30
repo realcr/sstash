4e2d04f3702b98d7f7c81c98012620c3706212a7
Fabian Affolter <fabian@affolter-engineering.ch>
2017-09-30 17:09:46 +0200

    Update for PEP8

--- setup.py
+++ setup.py
@@ -1,34 +1,39 @@
+"""
+sstash setup script.
+
+Based on https://python-packaging.readthedocs.io/en/latest/minimal.html
+"""
 from setuptools import setup
 
-# Based on
-# https://python-packaging.readthedocs.io/en/latest/minimal.html
 
 def readme():
-    with open('README.md','r') as fr:
+    with open('README.md', 'r') as fr:
         return fr.read()
 
 
-setup(name='sstash',
-        version='0.17',
-        description='A simple on-disk secure stash for secrets',
-        long_description=readme(),
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.5',
-            'Topic :: Security',
-        ],
-        keywords='sstash secure stash secret on-disk',
-        url='https://github.com/realcr/sstash',
-        author='real',
-        author_email='realcr@gmail.com',
-        license='MIT',
-        packages=['sstash'],
-        install_requires=[
-            'jsonschema',
-            'PyNaCl',
-        ],
-        setup_requires=['pytest-runner'],
-        tests_require=['pytest'],
-        include_package_data=True,
-        zip_safe=False)
+setup(
+    name='sstash',
+    version='0.17',
+    description='A simple on-disk secure stash for secrets',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Security',
+    ],
+    keywords='sstash secure stash secret on-disk',
+    url='https://github.com/realcr/sstash',
+    author='real',
+    author_email='realcr@gmail.com',
+    license='MIT',
+    packages=['sstash'],
+    install_requires=[
+       'jsonschema',
+       'PyNaCl',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    include_package_data=True,
+    zip_safe=False,
+)
